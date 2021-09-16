0b8b62ff58cd7e9499fcbf6a973984c1e04c3cf1
alantomcherian <atcherian101@gmail.com>
2021-09-16 20:41:42 +0530

    feat: uc1-Length of a line

--- Program.cs
+++ Program.cs
@@ -6,7 +6,16 @@ namespace day8
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("welcome to line comparision");
+            Console.WriteLine("enter coordinates of line (x1) ::");
+            int x1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter coordinates of line (y1) ::");
+            int y1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter coordinates of line (x2) ::");
+            int x2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter coordinates of line (y2) ::");
+            int y2 = int.Parse(Console.ReadLine());
+            float l1 = (float)Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+            Console.WriteLine(l1);
         }
     }
 }
